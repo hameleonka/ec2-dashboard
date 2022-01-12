067000260565c09a55fb0da2894bed93f945c598
Natalia Milovanova <hameleonka28@gmail.com>
2022-01-12 15:59:13 -0500

    Implements react table with faker's data

---
+++ src/helper.js
@@ -0,0 +1,17 @@
+import faker from 'faker';
+export function generateData() {
+  let dataArr = [];
+
+  for (let i = 0; i <= 100; i++) {
+    dataArr.push({
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      id: `i-${faker.datatype.uuid().slice(0, 9)}`,
+      type: `${faker.helpers.randomize(['C4', 'D2', 'G5', 'M6a', 'T2', 'X2gd'])}.${faker.helpers.randomize(['small', 'medium', 'large'])}`,
+      state: faker.helpers.randomize(['pending', 'running', 'stopping', 'stopped', 'shutting-down', 'terminated']),
+      az: `us-east-1-${faker.helpers.randomize(['atl', 'bos', 'chi', 'dfw', 'iah', 'mci', 'mia', 'msp', 'nyc', 'phl'])}-1a`,
+      publicIP: faker.internet.ip(),
+      privateIP: faker.internet.ip()
+    })
+  }
+  return dataArr;
+}
